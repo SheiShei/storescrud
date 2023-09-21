60cdcc4ee2de1c62432cdcc734fb21df59d70e6e
Sheira <sheira@wellola.com>
2023-09-21 22:56:57 +0800

    add routing for stores crud

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::controller(StoreController::class)->group(function () {
-        Route::get('/', 'index')->name('dashboard');
-    });
+    Route::get('/', [StoreController::class, 'index'])->name('dashboard');
+    Route::resource('store',StoreController::class)->except(['index']);
 });
 
 Route::middleware('auth')->group(function () {
